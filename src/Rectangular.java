496cacbc1bd6fd732315d26a85ac119b93fc2145
Jitendra Jain <Jitendra Jain>
2018-03-12 20:40:04 +0800

    Inital Java Commit

---
+++ src/Rectangular.java
@@ -0,0 +1,13 @@
+
+public class Rectangular()
+{
+	public static void main(Stting[] args)
+	{
+		int length = Integer.parseInt(args[0]);
+		int width= Integer.parseInt(args[1]);
+	}
+	
+	Rectangle myrect = new Rectange(length,width);
+	String output = String.format("***Your rectangle *** Area is: %d",myrect,getArea());
+	System.out.println(output);
+}
---
+++ src/rectangle.java
@@ -0,0 +1,21 @@
+public class Rectange
+{
+	public int length;
+	public int width;
+
+	public Rectangle(int length,int width)
+	{
+		this.length=length;
+		this.width=width;
+	}
+
+	public int getArea()
+	{
+		return length*width;
+	}
+
+	public getPerimeter()
+	{
+		return 2*(length+width);
+	}
+}
