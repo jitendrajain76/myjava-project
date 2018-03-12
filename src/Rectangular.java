76d161ecbf7a40979125edc6524483f45cfa59c0
Jitendra Jain <Jitendra Jain>
2018-03-13 00:51:01 +0800

    Updated files

--- src/Rectangular.java
+++ src/Rectangular.java
@@ -1,13 +1,14 @@
 
-public class Rectangular()
+public class Rectangular
 {
-	public static void main(Stting[] args)
+	public static void main(String[] args)
 	{
 		int length = Integer.parseInt(args[0]);
 		int width= Integer.parseInt(args[1]);
-	}
+	
 	
 	Srectangle myrect = new Srectange(length,width);
 	String output = String.format("***Your rectangle *** Area is: %d",myrect.getArea());
 	System.out.println(output);
+	}
 }
