bb05a933f62dc43e4e3367308759f0447dc15483
Jitendra Jain <Jitendra Jain>
2018-03-13 23:25:59 +0800

    COrrected java file

--- src/RectangeTest.java
+++ src/RectangeTest.java
@@ -2,7 +2,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class RectangleTest {
-  Rectangle myRectangle = new Rectangle(5,6);
+  Srectangle myRectangle = new Srectangle(5,6);
 
   @Test
   public void testGetArea() {
